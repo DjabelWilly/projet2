96a4f14435fe38ab5ba52e4a7bde7b15c5c9fa51
willy djabelkhir <willy.djabelkhir@office.weda.fr>
2025-06-03 15:00:27 +0200

    update

--- P2FixAnAppDotNetCode/Startup.cs
+++ P2FixAnAppDotNetCode/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
@@ -10,7 +7,8 @@ using Microsoft.Extensions.Options;
 using P2FixAnAppDotNetCode.Models;
 using P2FixAnAppDotNetCode.Models.Repositories;
 using P2FixAnAppDotNetCode.Models.Services;
-
+using System.Collections.Generic;
+using System.Globalization;
 namespace P2FixAnAppDotNetCode
 {
     public class Startup
@@ -41,7 +39,7 @@ namespace P2FixAnAppDotNetCode
                 .AddDataAnnotationsLocalization();
 
             services.Configure<RequestLocalizationOptions>(opts =>
-            { 
+            {
                 var supportedCultures = new List<CultureInfo>
                 {
                     new CultureInfo("en-GB"),
